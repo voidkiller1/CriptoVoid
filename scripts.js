6e6edb6400d02c24fd645fc67572a0088549c10f
voidkiller1 <hyago_nos@hotmail.com>
2024-05-31 13:56:41 -0300

    js att
    api faz a correção do valor a cada 3s

--- scripts.js
+++ scripts.js
@@ -30,39 +30,47 @@ document.addEventListener("DOMContentLoaded", function () {
         mouseLight.style.transform = `translate(${mouseX}px, ${mouseY}px)`;
     });
 
-    fetch('https://rest.coinapi.io/v1/exchangerate/BTC/USD', {
-        headers: {
-            'X-CoinAPI-Key': 'A87977FA-9FC3-4A03-8BC1-68EF634735AB'
-        }
-    })
-    .then(response => response.json())
-    .then(data => {
-        const bitcoinPriceElement = document.getElementById('btc-price');
-        bitcoinPriceElement.textContent = `R$ ${data.rate.toFixed(2)}`;
-    })
-    .catch(error => console.error('Erro:', error));
+    function updatePrices() {
+        fetch('https://rest.coinapi.io/v1/exchangerate/BTC/USD', {
+            headers: {
+                'X-CoinAPI-Key': 'A87977FA-9FC3-4A03-8BC1-68EF634735AB'
+            }
+        })
+        .then(response => response.json())
+        .then(data => {
+            const bitcoinPriceElement = document.getElementById('btc-price');
+            bitcoinPriceElement.textContent = `R$ ${data.rate.toFixed(2)}`;
+        })
+        .catch(error => console.error('Erro:', error));
 
-    fetch('https://rest.coinapi.io/v1/exchangerate/LTC/USD', {
-        headers: {
-            'X-CoinAPI-Key': 'A87977FA-9FC3-4A03-8BC1-68EF634735AB'
-        }
-    })
-    .then(response => response.json())
-    .then(data => {
-        const litecoinPriceElement = document.getElementById('ltc-price');
-        litecoinPriceElement.textContent = `R$ ${data.rate.toFixed(2)}`;
-    })
-    .catch(error => console.error('Erro:', error));
+        fetch('https://rest.coinapi.io/v1/exchangerate/LTC/USD', {
+            headers: {
+                'X-CoinAPI-Key': 'A87977FA-9FC3-4A03-8BC1-68EF634735AB'
+            }
+        })
+        .then(response => response.json())
+        .then(data => {
+            const litecoinPriceElement = document.getElementById('ltc-price');
+            litecoinPriceElement.textContent = `R$ ${data.rate.toFixed(2)}`;
+        })
+        .catch(error => console.error('Erro:', error));
 
-    fetch('https://rest.coinapi.io/v1/exchangerate/ETH/USD', {
-        headers: {
-            'X-CoinAPI-Key': 'A87977FA-9FC3-4A03-8BC1-68EF634735AB'
-        }
-    })
-    .then(response => response.json())
-    .then(data => {
-        const ethereumPriceElement = document.getElementById('ether-price');
-        ethereumPriceElement.textContent = `R$ ${data.rate.toFixed(2)}`;
-    })
-    .catch(error => console.error('Erro:', error));
+        fetch('https://rest.coinapi.io/v1/exchangerate/ETH/USD', {
+            headers: {
+                'X-CoinAPI-Key': 'A87977FA-9FC3-4A03-8BC1-68EF634735AB'
+            }
+        })
+        .then(response => response.json())
+        .then(data => {
+            const ethereumPriceElement = document.getElementById('ether-price');
+            ethereumPriceElement.textContent = `R$ ${data.rate.toFixed(2)}`;
+        })
+        .catch(error => console.error('Erro:', error));
+    }
+
+    // Chama a função updatePrices a cada 3 segundos
+    setInterval(updatePrices, 3000);
+
+    // Chama a função updatePrices uma vez imediatamente quando a página carrega
+    updatePrices();
 });
